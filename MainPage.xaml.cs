1799c45131189e26b9c9379a98b36d28871968ca
Blake Ross <blake.ross@amadeus.com>
2023-02-23 17:57:24 -0700

    Made logic more generic to support all platforms, adjusted debug output

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Maui.Controls.PlatformConfiguration;
+﻿using System.Diagnostics;
+using Microsoft.Maui.Controls.PlatformConfiguration;
 
 namespace DispatcherDemo;
 
@@ -13,20 +14,20 @@ public partial class MainPage : ContentPage
 
     private async void OnAwaitedTaskWithDispatcherButtonClicked(object sender, EventArgs e)
     {
-        Console.WriteLine("***********************************************************************");
-        Console.WriteLine("OnAwaitedTaskWithDispatcherButtonClicked");
-        Console.WriteLine("***********************************************************************");
-        Console.WriteLine("Dispatch should NOT yet be required.");
-        Console.WriteLine($"IsDispatchRequired: {Dispatcher.IsDispatchRequired}");
+        Debug.WriteLine("***********************************************************************");
+        Debug.WriteLine("* OnAwaitedTaskWithDispatcherButtonClicked");
+        Debug.WriteLine("***********************************************************************");
+        Debug.WriteLine("* Dispatch should NOT yet be required.");
+        Debug.WriteLine($"* IsDispatchRequired: {Dispatcher.IsDispatchRequired}");
 
         await Task.Run(async () =>
         {
-            Console.WriteLine(
-                "Running in a non-awaited Task - Dispatch SHOULD be required.");
-            Console.WriteLine($"IsDispatchRequired: {Dispatcher.IsDispatchRequired}");
+            Debug.WriteLine(
+                "* Running in a non-awaited Task - Dispatch SHOULD be required.");
+            Debug.WriteLine($"* IsDispatchRequired: {Dispatcher.IsDispatchRequired}");
 
-            Console.WriteLine(
-                "Attempting to push a new page using Dispatcher...  No exceptions should be thrown and navigation should complete.");
+            Debug.WriteLine(
+                "* Attempting to push a new page using Dispatcher...  No exceptions should be thrown and navigation should complete.");
 
             await Dispatcher.DispatchAsync(async () => { await navigateToNewPage(); });
         });
@@ -34,11 +35,11 @@ public partial class MainPage : ContentPage
 
     private async void OnMainThreadNoDispatcherButtonClicked(object sender, EventArgs e)
     {
-        Console.WriteLine("****************************************************************************");
-        Console.WriteLine("OnMainThreadNoDispatcherButtonClicked");
-        Console.WriteLine("****************************************************************************");
+        Debug.WriteLine("****************************************************************************");
+        Debug.WriteLine("* OnMainThreadNoDispatcherButtonClicked");
+        Debug.WriteLine("****************************************************************************");
 
-        Console.WriteLine("Attempting to push a new page on the main thread WITHOUT Tasks/Dispatcher... No exceptions should be thrown and navigation should complete.");
+        Debug.WriteLine("* Attempting to push a new page on the main thread WITHOUT Tasks/Dispatcher... No exceptions should be thrown and navigation should complete.");
         await navigateToNewPage();
     }
 
@@ -48,94 +49,42 @@ public partial class MainPage : ContentPage
 
     private async void OnAwaitedTaskNoDispatcherButtonClicked(object sender, EventArgs e)
     {
-        Console.WriteLine("***********************************************************************");
-        Console.WriteLine("OnAwaitedTaskNoDispatcherButtonClicked");
-        Console.WriteLine("***********************************************************************");
-        Console.WriteLine("Dispatch should NOT yet be required.");
-        Console.WriteLine($"IsDispatchRequired: {Dispatcher.IsDispatchRequired}");
+        Debug.WriteLine("***********************************************************************");
+        Debug.WriteLine("* OnAwaitedTaskNoDispatcherButtonClicked");
+        Debug.WriteLine("***********************************************************************");
+        Debug.WriteLine("* Dispatch should NOT yet be required.");
+        Debug.WriteLine($"* IsDispatchRequired: {Dispatcher.IsDispatchRequired}");
 
         await Task.Run(async () =>
         {
-            Console.WriteLine(
-                "Running in a non-awaited Task - Dispatch SHOULD be required.");
-            Console.WriteLine($"IsDispatchRequired: {Dispatcher.IsDispatchRequired}");
+            Debug.WriteLine(
+                "* Running in a non-awaited Task - Dispatch SHOULD be required.");
+            Debug.WriteLine($"* IsDispatchRequired: {Dispatcher.IsDispatchRequired}");
 
-            Console.WriteLine("Attempting to push a new page WITHOUT Dispatcher...  An AndroidRuntimeException should be thrown in Android and a UIKitThreadAccessException in iOS. Navigation is messed up going forward...");
+            Debug.WriteLine("* Attempting to push a new page WITHOUT Dispatcher...  An AndroidRuntimeException should be thrown in Android and a UIKitThreadAccessException in iOS. Navigation is messed up going forward...");
 
-#if ANDROID
-            try
-            {
-                await navigateToNewPage();
-            }
-            catch (Android.Util.AndroidRuntimeException ex)
-            {
-                Console.WriteLine("AndroidRuntimeException WAS thrown in Android.");
-                await Dispatcher.DispatchAsync(async () =>
-                {
-                    await DisplayAlert(ex.GetType().Name, ex.Message, "Thanks.");
-                });
-            }
-#elif IOS
-            try
-            {
-                await navigateToNewPage();
-            }
-            catch (UIKit.UIKitThreadAccessException ex)
-            {
-                Console.WriteLine("UIKitThreadAccessException WAS thrown in iOS.");
-                await Dispatcher.DispatchAsync(async () =>
-                {
-                    await DisplayAlert(ex.GetType().Name, ex.Message, "Thanks.");
-                });
-            }
-#endif
+            await navigateToNewPage();
         });
     }
 
     private void OnNonAwaitedTaskNoDispatcherButtonClicked(object sender, EventArgs e)
     {
-        Console.WriteLine("***********************************************************************");
-        Console.WriteLine("OnNonAwaitedTaskNoDispatcherButtonClicked");
-        Console.WriteLine("***********************************************************************");
+        Debug.WriteLine("***********************************************************************");
+        Debug.WriteLine("* OnNonAwaitedTaskNoDispatcherButtonClicked");
+        Debug.WriteLine("***********************************************************************");
 
-        Console.WriteLine("Dispatch should NOT yet be required.");
-        Console.WriteLine($"IsDispatchRequired: {Dispatcher.IsDispatchRequired}");
+        Debug.WriteLine("* Dispatch should NOT yet be required.");
+        Debug.WriteLine($"* IsDispatchRequired: {Dispatcher.IsDispatchRequired}");
 
         _ = Task.Run(async () =>
         {
-            Console.WriteLine(
-                "Running in a non-awaited Task - Dispatch should be required.");
-            Console.WriteLine($"IsDispatchRequired: {Dispatcher.IsDispatchRequired}");
+            Debug.WriteLine(
+                "* Running in a non-awaited Task - Dispatch should be required.");
+            Debug.WriteLine($"* IsDispatchRequired: {Dispatcher.IsDispatchRequired}");
         
-            Console.WriteLine("Attempting to push a new page WITHOUT Dispatcher...  No exceptions seem to throw in Android, but a UIKitThreadAccessException is thrown in iOS. Navigation is messed up going forward...");
+            Debug.WriteLine("* Attempting to push a new page WITHOUT Dispatcher...  No exceptions seem to throw in Android, but a UIKitThreadAccessException is thrown in iOS. Navigation is messed up going forward...");
 
-#if ANDROID
-            try
-            {
-                await navigateToNewPage();
-            }
-            catch (Android.Util.AndroidRuntimeException ex)
-            {
-                Console.WriteLine("AndroidRuntimeException WAS thrown in Android.");
-                await Dispatcher.DispatchAsync(async () =>
-                {
-                    await DisplayAlert(ex.GetType().Name, ex.Message, "Thanks.");
-                });
-            }
-#elif IOS
-            try
-            {
-                await navigateToNewPage();
-            }
-            catch (UIKit.UIKitThreadAccessException ex)
-            {
-                Console.WriteLine("UIKitThreadAccessException WAS thrown in iOS.");
-                await Dispatcher.DispatchAsync(async () =>
-                {
-                    await DisplayAlert(ex.GetType().Name, ex.Message, "Thanks.");
-                });
-            }
-#endif
+            await navigateToNewPage();
         });
     }
 
@@ -143,7 +92,19 @@ public partial class MainPage : ContentPage
 
     private async Task navigateToNewPage()
     {
-        await Shell.Current.GoToAsync("/TestPage");
+        try
+        {
+            await Shell.Current.GoToAsync("/TestPage");
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"* {ex.GetType().Name} was thrown in {DeviceInfo.Current.Platform}.");
+            await Dispatcher.DispatchAsync(async () =>
+            {
+                await DisplayAlert(ex.GetType().Name, ex.Message, "Thanks.");
+            });
+        }
+        
     }
 }
 
